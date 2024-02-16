4a6eb393cd8688e084b174508c32d7a59e1c9823
mounir-mansi <114388491+mounir-mansi@users.noreply.github.com>
2024-02-16 16:00:16 +0100

    Update GameDetails.jsx

--- src/components/GameDescription/GameDetails.jsx
+++ src/components/GameDescription/GameDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './gameDetails.css'
+import './GameDetails.css'
 
 const GameDetails = () => {
   const { id } = useParams();
