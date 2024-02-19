e24cb90ec3edc0f64c9fb700e5e2396a66833ef0
mounir-mansi <114388491+mounir-mansi@users.noreply.github.com>
2024-02-19 09:21:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Configuration du type MIME pour les fichiers JavaScript
-    mimeTypes: {
-      '.js': 'text/javascript'
-    }
-  }
-});
+})
 
